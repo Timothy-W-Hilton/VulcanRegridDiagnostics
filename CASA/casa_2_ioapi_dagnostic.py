b1a920c5f41c57a2a3f74a05fb95c3c45ac8f6d4
Timothy W. Hilton <thilton@ucmerced.edu>
2017-03-24 14:12:27 -0700

    I/O API diagnostics: show 9-km CASA in third panel

--- CASA/casa_2_ioapi_dagnostic.py
+++ CASA/casa_2_ioapi_dagnostic.py
@@ -39,17 +39,18 @@ def get_casa_raw(pt_lat=37, pt_lon=-121.25):
     return x, y, lon[x, y], lat[x, y], pt_gee
 
 
-def get_casa_raw_ioapi(pt_lat=37, pt_lon=-121.25):
+def get_casa_ioapi(fname_latlon, fname_gee, pt_lat=37, pt_lon=-121.25):
     """read CASA I/O API data, find grid center nearest to arguments
 
     This expects the non-regridded CASA data in I/O API format.  The
     longitude and latitude coordinates for the I/O API CASA grid are
     read from the output of latlon
     (https://www.cmascenter.org/ioapi/documentation/all_versions/html/LATLON.html).
-    The latlon output is expected to be in the file
-    "casa_raw_grid.nc".
 
     ARGS:
+    fname_latlon (string): full path to latlon output
+    fname_gee (string): full path to the I/O API file containing GEE.
+        The GEE variable must be CO2_FLUX
     pt_lat: float; latitude coordinate of the point to locate
     pt_lon: float; longitude coordinate of the point to locate
 
@@ -60,39 +61,56 @@ def get_casa_raw_ioapi(pt_lat=37, pt_lon=-121.25):
     lat[x, y] (float): the latitude of [x, y] in the CASA I/O API grid
     pt_gee (np.ndarray): CASA I/O API GEE time series for [x, y]
     """
-    nc = netCDF4.Dataset(os.path.join('/', 'global', 'homes', 't',
-                                      'twhilton', 'Code', 'Regrid',
-                                      'VulcanRegrid', 'CASA',
-                                      'casa_raw_grid.nc'))
+    nc = netCDF4.Dataset(fname_latlon)
     lon = nc.variables['LON'][0, 0, ...]
     lat = nc.variables['LAT'][0, 0, ...]
     nc.close()
     x, y = find_nearest_stem_xy(pt_lon, pt_lat, lon, lat)
-    # x, y = (127, 47)
-    nc = netCDF4.Dataset(os.path.join('/global', 'homes', 't',
-                                      'twhilton', 'Code', 'Regrid',
-                                      'VulcanRegrid', 'CASA',
-                                      'tim_test3.nc'))
+    # x, y = (24, 28)
+    nc = netCDF4.Dataset(fname_gee)
     pt_gee = nc.variables['CO2_FLUX'][:, 0, x, y]
     return x, y, lon[x, y], lat[x, y], pt_gee
 
 
 if __name__ == "__main__":
     x_raw, y_raw, lon_raw, lat_raw, gee_raw = get_casa_raw()
-    x_ioapi, y_ioapi, lon_ioapi, lat_ioapi, gee_ioapi = get_casa_raw_ioapi()
-
-    fig, ax = plt.subplots(nrows=2, ncols=1)
+    latlon_casa_raw = os.path.join('/', 'global', 'homes', 't',
+                                   'twhilton', 'Code', 'Regrid',
+                                   'VulcanRegrid', 'CASA',
+                                   'casa_raw_grid.nc')
+    gee_casa_raw_ioapi = os.path.join('/global', 'homes', 't',
+                                      'twhilton', 'Code', 'Regrid',
+                                      'VulcanRegrid', 'CASA',
+                                      'tim_test3.nc')
+    latlon_casa_9km = os.path.join('/', 'global', 'homes', 't',
+                                   'twhilton', 'Code', 'Regrid',
+                                   'VulcanRegrid', 'CASA',
+                                   'casa_grid.nc')
+    gee_casa_9km = os.path.join("/", "project", "projectdirs",
+                                "m2319", "transfer",
+                                "CASA_GEE_perm2_stem9km_ioapi.nc")
+    (x_ioapi, y_ioapi, lon_ioapi,
+     lat_ioapi, gee_ioapi) = get_casa_ioapi(latlon_casa_raw,
+                                            gee_casa_raw_ioapi)
+    (x_9km, y_9km, lon_9km,
+     lat_9km, gee_9km) = get_casa_ioapi(latlon_casa_9km,
+                                            gee_casa_9km)
+    fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(8, 16))
     ax[0].plot(gee_raw)
     ax[0].set_title('CASA raw ({}, {}) ({}, {})'.format(
         x_raw, y_raw, lon_raw, lat_raw))
     ax[1].plot(gee_ioapi)
     ax[1].set_title('CASA native grid I/O API ({}, {}) ({}, {})'.format(
         x_ioapi, y_ioapi, lon_ioapi, lat_ioapi))
+    ax[2].plot(gee_ioapi)
+    ax[2].set_title('CASA 9 KM I/O API ({}, {}) ({}, {})'.format(
+        x_9km, y_9km, lon_9km, lat_9km))
     for this_ax in ax:
         this_ax.set_ylabel('GEE')
     ax[0].set_ylim((0, -3.0e-7))
     ax[1].set_ylim((0, -3.0e11))
-    ax[1].set_xlabel('tstep')
+    ax[2].set_ylim((0, -3.0e11))
+    ax[2].set_xlabel('tstep')
     fname = '{}_{}.pdf'.format(x_ioapi, y_ioapi)
     print 'saving {}'.format(fname)
     fig.savefig(fname)
